6bad23126e782399b335d9dfb62b6108a5342467
V1merX <vimertop73@gmail.com>
2025-08-20 21:38:27 +0300

    add server; handler; middleware; test user handler

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,11 +1,23 @@
 package app
 
 import (
+	"context"
+	"errors"
+	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/V1merX/upserv-api/internal/config"
+	"github.com/V1merX/upserv-api/internal/server"
+	"github.com/V1merX/upserv-api/internal/service"
+	transport "github.com/V1merX/upserv-api/internal/transport/rest"
+	auth "github.com/V1merX/upserv-api/pkg/auth/jwt"
 	log "github.com/sirupsen/logrus"
 )
+
+const timeout = 5 * time.Second
 
 func init() {
 	log.SetFormatter(&log.JSONFormatter{})
@@ -25,8 +37,34 @@ func Run(configPath string) {
 
 	log.SetLevel(logLVL)
 
-	// err = a2s.Run("95.213.255.150", 27415)
-	// if err != nil {
-	// 	log.Error(err)
-	// }
+	tokenManager, err := auth.NewManager(cfg.Auth.JWT.SigningKey)
+	if err != nil {
+		log.Error(err)
+		return
+	}
+
+	services := service.NewServices()
+	handlers := transport.NewHandler(services, *tokenManager)
+
+	srv := server.NewServer(cfg, handlers.Init(cfg))
+
+	go func() {
+		if err := srv.Run(); !errors.Is(err, http.ErrServerClosed) {
+			log.Errorf("error occurred while running http server: %s\n", err.Error())
+		}
+	}()
+
+	log.Info("server started")
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
+
+	<-quit
+
+	ctx, shutdown := context.WithTimeout(context.Background(), timeout)
+	defer shutdown()
+
+	if err := srv.Stop(ctx); err != nil {
+		log.Errorf("failed to stop server: %v", err)
+	}
 }
---
+++ internal/transport/rest/v1/handler.go
@@ -0,0 +1,26 @@
+package v1
+
+import (
+	"github.com/V1merX/upserv-api/internal/service"
+	auth "github.com/V1merX/upserv-api/pkg/auth/jwt"
+	"github.com/gin-gonic/gin"
+)
+
+type Handler struct {
+	services     *service.Services
+	tokenManager auth.TokenManager
+}
+
+func NewHandler(services *service.Services, tokenManager auth.TokenManager) *Handler {
+	return &Handler{
+		services:     services,
+		tokenManager: tokenManager,
+	}
+}
+
+func (h *Handler) Init(api *gin.RouterGroup) {
+	v1 := api.Group("/v1")
+	{
+		h.initUsersRoutes(v1)
+	}
+}
